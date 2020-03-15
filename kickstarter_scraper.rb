76f457f85574253558450dd278e75f148ca3a55e
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-15 20:53:51 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,16 +1,14 @@
 # require libraries/modules here
 require 'nokogiri'
 require 'pry'
+#projects: kickstarter.css("li.project.grid_4")
 
 
 def create_project_hash
-
   html = File.read('fixtures/kickstarter.html')
-
   kickstarter = Nokogiri::HTML(html)
   binding.pry
   #projects: kickstarter.css("li.project.grid_4")
-
 end
 
 create_project_hash
