2a167aba50f48342222f1815c4e2afb51f3e6904
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-15 21:44:07 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,10 +12,10 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  
+
 def create_project_hash
   projects = {}
-  
+
 # Iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
